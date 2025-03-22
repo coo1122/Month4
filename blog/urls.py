78721679ecfefdae0c39618d6b97a4b051e86241
coo1122 <adiletusenov50@gmail.com>
2025-03-22 12:43:57 -0700

    HW 4

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import test_view, home_page_view, post_list_view
+from posts.views import test_view, home_page_view, post_list_view, post_detail_view, post_create_view
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,4 +25,6 @@ urlpatterns = [
     path('test/', test_view),
     path('', home_page_view),
     path('posts/', post_list_view),
+    path('posts/<int:post_id>/', post_detail_view),
+    path('posts/create/', post_create_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import random
 from posts.models import Post
+from posts.forms import PostCreateform, PostCreateform2
 
 
 def test_view(request):
@@ -9,8 +10,33 @@ def test_view(request):
 
 
 def home_page_view(request):
-    return render(request, "base.html")
+    if request.method == "GET":
+        return render(request, "base.html")
 
 def post_list_view(request):
     posts = Post.objects.all()
-    return render(request, "posts/post_list.html", context={"posts": posts})
+    if request.method == "GET":
+        return render(request, "posts/post_list.html", context={"posts": posts})
+
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "posts/post_detail.html", context={"post": post})
+
+
+def post_create_view(request):
+    if request.method == "GET":
+        form = PostCreateform2()
+        return render(request, "posts/post_create.html", context={"form": form})
+    if request.method == "POST":
+        form = PostCreateform2(request.POST, request.FILES)
+        if not form.is_valid():
+            return render(request, "posts/post_create.html", context={"form": form})
+        elif form.is_valid():
+            form.save()
+            # title = form.cleaned_data.get("title")
+            # content = form.cleaned_data.get("content")
+            # image = form.cleaned_data.get("image")
+            # post = Post.objects.create(title=title, content=content, image=image)
+        # if post:
+            return redirect("/posts/")
